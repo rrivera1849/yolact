d79f4844f823e984d6b3bf523657572cb1f73a4f
Rivera Soto, Rafael Alberto <riverasoto1@llnl.gov>
2020-04-15 20:56:13 -0700

    First pass at adding MobilenetV2 as a backbone.

--- backbone.py
+++ backbone.py
@@ -443,7 +443,150 @@ class VGGBackbone(nn.Module):
         self.channels.append(self.in_channels)
         self.layers.append(layer)
         
-                
+
+def _make_divisible(v, divisor, min_value=None):
+    """
+    Adapted from torchvision.models.mobilenet._make_divisible.
+    """
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+class ConvBNReLU(nn.Sequential):
+    """
+    Adapted from torchvision.models.mobilenet.ConvBNReLU
+    """
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU6(inplace=True)
+        )
+
+
+class InvertedResidual(nn.Module):
+    """
+    Adapted from torchvision.models.mobilenet.InvertedResidual
+    """
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(round(inp * expand_ratio))
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        layers = []
+        if expand_ratio != 1:
+            # pw
+            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
+        layers.extend([
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(oup),
+        ])
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2Backbone(nn.Module):
+    """
+    Adapted from torchvision.models.mobilenet.MobileNetV2
+    """
+    def __init__(self,
+                 num_classes=1000,
+                 width_mult=1.0,
+                 inverted_residual_setting=None,
+                 round_nearest=8,
+                 block=InvertedResidual):
+        super(MobileNetV2, self).__init__()
+
+        input_channel = 32
+        last_channel = 1280
+
+        self.layers = nn.ModuleList()
+
+        if inverted_residual_setting is None:
+            inverted_residual_setting = [
+                # t, c, n, s
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+
+        # only check the first element, assuming user knows t,c,n,s are required
+        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+            raise ValueError("inverted_residual_setting should be non-empty "
+                             "or a 4-element list, got {}".format(inverted_residual_setting))
+
+        # building first layer
+        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
+        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+        self.layers.append(ConvBNReLU(3, input_channel, stride=2))
+
+        # building inverted residual blocks
+        for t, c, n, s in inverted_residual_setting:
+            input_channel = self._make_layer(input_channel, width_mult, round_nearest, t, c, n, s, block)
+
+        # building last several layers
+        self.layers.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
+
+        # building classifier
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(self.last_channel, num_classes),
+        )
+
+
+    def _make_layer(self, input_channel, width_mult, round_nearest, t, c, n, s, block):
+        """A layer is a combination of inverted residual blocks"""
+        layers = []
+        output_channel = _make_divisible(c * width_mult, round_nearest)
+
+        for i in range(n):
+            stride = s if i == 0 else 1
+            layers.append(block(input_channel, output_channel, stride, expand_ratio=t))
+            input_channel = output_channel
+
+        self.layers.append(nn.Sequential(*layers))
+        return input_channel
+
+
+    def forward(self, x):
+        """ Returns a list of convouts for each layer. """
+        # This exists since TorchScript doesn't support inheritance, so the superclass method
+        # (this one) needs to have a name other than `forward` that can be accessed in a subclass
+        outs = []
+
+        for idx, layer in enumerate(self.layers):
+            x = layer(x)
+            
+            outs.append(x)
+        
+        return tuple(outs)
+
+
+    def add_layer(self, conv_channels=1280, t=1, c=1280, n=1, s=2):
+        """TODO: Need to make sure that this works as intended.
+        """
+        self._make_layer(conv_channels, 1.0, 8, t, c, n, s, InvertedResidual)
 
 
 def construct_backbone(cfg):
